af3e4763c383e55752f7c75394a11880109fd2c6
gkum4 <gustavokumasawa@gmail.com>
2021-08-25 10:45:55 -0300

    Feat project structure and DegreeScene

---
+++ TimMaia/Stories/LiseMeitner/Scenes/DegreeScene/DegreeScene.swift
@@ -0,0 +1,43 @@
+//
+//  DegreeScene.swift
+//  TimMaia
+//
+//  Created by Gustavo Kumasawa on 25/08/21.
+//
+
+import SpriteKit
+
+class DegreeScene: SKScene {
+  override func didMove(to view: SKView) {
+  }
+  
+  func touchDown(atPoint pos : CGPoint) {
+  }
+  
+  func touchMoved(toPoint pos : CGPoint) {
+  }
+  
+  func touchUp(atPoint pos : CGPoint) {
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+      // Called before each frame is rendered
+  }
+}
