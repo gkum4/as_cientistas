65dd799db520376365b23037cd9f7f5d94b798e3
João Pedro Picolo <picolojoaop@gmail.com>
2021-08-27 20:13:40 -0300

    Adds basic periodic table scene animation

--- TimMaia/ViewController.swift
+++ TimMaia/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
+  
+  override func loadView() {
+    if let scene = SKScene(fileNamed: "PeriodicTableScene") {
+      let sceneView = SKView()
+      scene.scaleMode = .aspectFit
+      sceneView.presentScene(scene)
 
-
+      self.view = sceneView
+    }
+  }
 }
 
