eaa8731ca063ca4ec4359a5ebf14c09b5d0d9adb
Frederico Westphalen Mendes Machado <fwestphalendesign@gmail.com>
2021-09-17 01:17:11 -0300

    Mais ajustes da home

--- TimMaia/Stories/LiseMeitner/Home/CardTableViewCell.swift
+++ TimMaia/Stories/LiseMeitner/Home/CardTableViewCell.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class CardTableViewCell: UITableViewCell {
+
+    
     @IBOutlet weak var thumbnails: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        
+//        thumbnails.layer.shadowColor = UIColor.red.cgColor
+//        thumbnails.layer.shadowOpacity = 1
+//        thumbnails.layer.shadowOffset = .zero
+//        thumbnails.layer.shadowRadius = 30
+//
+//
+//        self.clipsToBounds = false
+//        thumbnails.clipsToBounds = false
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,5 +32,20 @@ class CardTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0))
+        
+        
+        
+    }
 
 }
+
+
+
+
+
